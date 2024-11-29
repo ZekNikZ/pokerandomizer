b3a1bef16e62ae6c018a75da51fba9c047c66226
Matthew McCaskill <mattrmccaskill@gmail.com>
2024-11-28 17:51:22 -0700

    change green to white

--- src/app/page.client.tsx
+++ src/app/page.client.tsx
@@ -32,7 +32,7 @@ export default function HomePage() {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-36 overflow-hidden bg-gray-800">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-48 overflow-hidden bg-gray-800">
       {teams.map((team) => (
         <TeamPokeballs key={team.uuid} teamId={team.uuid} />
       ))}
